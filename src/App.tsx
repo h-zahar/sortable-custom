c6fa19908de0bdf548ed6139eee7847b45d16de9
h-zahar <hz.rifat@gmail.com>
2024-01-29 22:01:30 +0600

    Fixed TS Issues and Refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,292 +1,62 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import MakeSortable from "./components/MakeSortable";
-import TakeSortable from "./components/TakeSortable";
 
 function App() {
-  const [array, setArray] = useState([1, 2, 3, 4, 5]);
-  const [translate, setTranslate] = useState({ x: 0, y: 0 });
-  const [indicatorPosition, setIndicatorPosition] = useState({ x: 0, y: 0 });
-  const [isDone, setIsDone] = useState(false);
+  const array = [1, 2, 3, 4, 5];
 
-  const [items, setItems] = useState([
-    <div
-      // id={"1"}
-      style={{
-        width: 300,
-        height: 50,
-        marginBottom: 50,
-        background: "lightblue",
-        color: "black",
-        // transform: `translate(${translate.x}px, ${translate.y}px)`,
-        transform: `translate(0px, 0px)`,
-        border: "1px solid black",
-        position: "relative",
-      }}
-    >
-      <div style={{ display: "flex", height: "100%" }}>
-        <div
-          // id={"1" + "-handler"}
-          className="handler"
-          style={{
-            height: 50,
-            width: 50,
-            border: "1px solid blue",
-            cursor: "grab",
-          }}
-          // onMouseDown={handleMouseDown}
-        ></div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-            userSelect: "none",
-          }}
-        >
-          1
+  const [items, setItems] = useState(
+    array.map((elem) => (
+      <div
+        style={{
+          width: 300,
+          height: 50,
+          marginBottom: 50,
+          background: "lightblue",
+          color: "black",
+          transform: `translate(0px, 0px)`,
+          border: "1px solid black",
+          position: "relative",
+        }}
+      >
+        <div style={{ display: "flex", height: "100%" }}>
+          <div
+            className="handler"
+            style={{
+              height: 50,
+              width: 50,
+              border: "1px solid blue",
+              cursor: "grab",
+            }}
+          ></div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              width: "100%",
+              userSelect: "none",
+            }}
+          >
+            {elem}
+          </div>
         </div>
       </div>
-    </div>,
-    <div
-      // id={"2"}
-      style={{
-        width: 300,
-        height: 50,
-        marginBottom: 50,
-        background: "lightblue",
-        color: "black",
-        // transform: `translate(${translate.x}px, ${translate.y}px)`,
-        transform: `translate(0px, 0px)`,
-        border: "1px solid black",
-        position: "relative",
-      }}
-    >
-      <div style={{ display: "flex", height: "100%" }}>
-        <div
-          // id={"2" + "-handler"}
-          className="handler"
-          style={{
-            height: 50,
-            width: 50,
-            border: "1px solid blue",
-            cursor: "grab",
-          }}
-          // onMouseDown={handleMouseDown}
-        ></div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-            userSelect: "none",
-          }}
-        >
-          2
-        </div>
-      </div>
-    </div>,
-    <div
-      // id={"3"}
-      style={{
-        width: 300,
-        height: 50,
-        marginBottom: 50,
-        background: "lightblue",
-        color: "black",
-        // transform: `translate(${translate.x}px, ${translate.y}px)`,
-        transform: `translate(0px, 0px)`,
-        border: "1px solid black",
-        position: "relative",
-      }}
-    >
-      <div style={{ display: "flex", height: "100%" }}>
-        <div
-          // id={"3" + "-handler"}
-          className="handler"
-          style={{
-            height: 50,
-            width: 50,
-            border: "1px solid blue",
-            cursor: "grab",
-          }}
-          // onMouseDown={handleMouseDown}
-        ></div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-            userSelect: "none",
-          }}
-        >
-          3
-        </div>
-      </div>
-    </div>,
-    <div
-      // id={"4"}
-      style={{
-        width: 300,
-        height: 50,
-        marginBottom: 50,
-        background: "lightblue",
-        color: "black",
-        // transform: `translate(${translate.x}px, ${translate.y}px)`,
-        transform: `translate(0px, 0px)`,
-        border: "1px solid black",
-        position: "relative",
-      }}
-    >
-      <div style={{ display: "flex", height: "100%" }}>
-        <div
-          // id={"4" + "-handler"}
-          className="handler"
-          style={{
-            height: 50,
-            width: 50,
-            border: "1px solid blue",
-            cursor: "grab",
-          }}
-          // onMouseDown={handleMouseDown}
-        ></div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-            userSelect: "none",
-          }}
-        >
-          4
-        </div>
-      </div>
-    </div>,
-    <div
-      // id={"5"}
-      style={{
-        width: 300,
-        height: 50,
-        marginBottom: 50,
-        background: "lightblue",
-        color: "black",
-        // transform: `translate(${translate.x}px, ${translate.y}px)`,
-        transform: `translate(0px, 0px)`,
-        border: "1px solid black",
-        position: "relative",
-      }}
-    >
-      <div style={{ display: "flex", height: "100%" }}>
-        <div
-          // id={"5" + "-handler"}
-          className="handler"
-          style={{
-            height: 50,
-            width: 50,
-            border: "1px solid blue",
-            cursor: "grab",
-          }}
-          // onMouseDown={handleMouseDown}
-        ></div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-            userSelect: "none",
-          }}
-        >
-          5
-        </div>
-      </div>
-    </div>,
-  ]);
-
-  // useEffect(() => {
-  //   setItems(
-  //     array.map((id) => (
-  //       <div
-  //         id={id.toString()}
-  //         style={{
-  //           width: 300,
-  //           height: 50,
-  //           marginBottom: 50,
-  //           background: "lightblue",
-  //           color: "black",
-  //           // transform: `translate(${translate.x}px, ${translate.y}px)`,
-  //           transform: `translate(0px, 0px)`,
-  //           border: "1px solid black",
-  //           position: "relative",
-  //         }}
-  //       >
-  //         <div style={{ display: "flex", height: "100%" }}>
-  //           <div
-  //             id={id.toString() + "-handler"}
-  //             className="handler"
-  //             style={{
-  //               height: 50,
-  //               width: 50,
-  //               border: "1px solid blue",
-  //               cursor: "grab",
-  //             }}
-  //             // onMouseDown={handleMouseDown}
-  //           ></div>
-  //           <div
-  //             style={{
-  //               display: "flex",
-  //               justifyContent: "center",
-  //               alignItems: "center",
-  //               height: "100%",
-  //               width: "100%",
-  //               userSelect: "none",
-  //             }}
-  //           >
-  //             {id}
-  //           </div>
-  //         </div>
-  //       </div>
-  //     ))
-  //   );
-  // }, [array]);
+    ))
+  );
 
   return (
     <div>
       <p style={{ userSelect: "none" }}>Sortable Custom</p>
-      <div
-        id="indicator"
-        style={{
-          display: "none",
-          position: "absolute",
-          top: indicatorPosition.y,
-          left: "calc(50% - 150px)",
-          height: "5px",
-          background: "blue",
-          width: 300,
-        }}
-      ></div>
 
-      {/* {items.length && ( */}
-      <TakeSortable
-        translate={translate}
-        setTranslate={setTranslate}
+      <MakeSortable
         array={items}
-        indicatorPosition={indicatorPosition}
-        setIndicatorPosition={setIndicatorPosition}
         onSort={(items) => setItems(items)}
+        isIndicator={true}
       >
         {items}
-      </TakeSortable>
-      {/* )} */}
+      </MakeSortable>
     </div>
   );
 }
--- src/components/MakeSortable.tsx
+++ src/components/MakeSortable.tsx
@@ -1,16 +1,149 @@
-import { useState } from "react";
-import Box from "./Box";
+import { ReactElement, useEffect, useState } from "react";
 
 const MakeSortable = ({
-  items,
-  setItems,
+  array,
+  onSort,
+  isIndicator = true,
+  children,
 }: {
-  items: number[];
-  setItems: (items: number[]) => void;
+  array: ReactElement[];
+  onSort: (array: ReactElement[]) => void;
+  isIndicator?: boolean;
+  children: ReactElement[];
 }) => {
   const [indicatorPosition, setIndicatorPosition] = useState({ x: 0, y: 0 });
+
+  const doesHandlerExist = [...document.getElementsByClassName("handler")]
+    .length;
+
+  const doesContainerExist = document.getElementById("box");
+
+  useEffect(
+    () => {
+      [...document.getElementsByClassName("handler")].forEach((elem) =>
+        (elem as HTMLDivElement).addEventListener("mousedown", handleMouseDown)
+      );
+      document.getElementById("box") &&
+        [...document.getElementById("box")!.children].map((elem, i) =>
+          elem.setAttribute("index", i + "")
+        );
+    },
+    // eslint-disable-next-line
+    [children, doesHandlerExist, doesContainerExist]
+  );
+
+  if (!children.length) return <></>;
+
+  let currentElement = null;
+  let index = -1;
+  let startPosition = { x: 0, y: 0 };
+
+  const handleMouseDown = (e: MouseEvent) => {
+    startPosition = { x: e.clientX, y: e.clientY };
+
+    currentElement = (e.currentTarget! as HTMLDivElement)!.parentNode
+      ?.parentNode;
+
+    index = Number(
+      ((e.currentTarget! as HTMLDivElement)!.parentNode
+        ?.parentNode as HTMLDivElement)!.getAttribute("index")
+    );
+
+    (currentElement as HTMLDivElement)!.style.zIndex = "100";
+    (currentElement as HTMLDivElement)!.style.opacity = "0.6";
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    currentElement!.style.opacity = "0.6";
+    currentElement!.style.transform = `translate(${
+      e.clientX - startPosition.x
+    }px, ${e.clientY - startPosition.y}px)`;
+
+    Math.floor(Math.abs(startPosition.y - e.clientY) / 100) === 0 &&
+      (document.getElementById("indicator")!.style.display! = "none");
+
+    if (Math.floor(Math.abs(startPosition.y - e.clientY) / 100) !== 0) {
+      isIndicator &&
+        setTimeout(
+          () =>
+            (document.getElementById("indicator")!.style.display! = "block"),
+          10
+        );
+      isIndicator &&
+        setIndicatorPosition({
+          x: indicatorPosition.x,
+          y:
+            startPosition.y - e.clientY < 0
+              ? Number(
+                  document
+                    .getElementById("box")
+                    ?.children[
+                      index +
+                        Math.max(
+                          Math.floor(
+                            Math.abs(startPosition.y - e.clientY) / 100
+                          )
+                        )
+                    ]?.getBoundingClientRect().y
+                ) + 75
+              : Number(
+                  document
+                    .getElementById("box")
+                    ?.children[
+                      index -
+                        Math.min(
+                          Math.floor(
+                            Math.abs(startPosition.y - e.clientY) / 100
+                          ),
+                          index
+                        )
+                    ]?.getBoundingClientRect().y
+                ) - 25,
+        });
+    }
+  };
+
+  const handleMouseUp = (e: MouseEvent) => {
+    setTimeout(
+      () => (document.getElementById("indicator")!.style.display! = "none"),
+      20
+    );
+
+    currentElement!.style.opacity = "1";
+
+    currentElement!.style.transform = `translate(0px, 0px)`;
+
+    currentElement!.style.zIndex = "0";
+
+    const tempArr = [...array];
+    const elem = tempArr.splice(index, 1);
+    startPosition.y - e.clientY < 0
+      ? tempArr.splice(
+          index +
+            Math.max(Math.floor(Math.abs(startPosition.y - e.clientY) / 100)),
+          0,
+          elem[0]
+        )
+      : tempArr.splice(
+          index -
+            Math.min(
+              Math.floor(Math.abs(startPosition.y - e.clientY) / 100),
+              index
+            ),
+          0,
+          elem[0]
+        );
+    onSort(tempArr);
+
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  };
+
   return (
-    <>
+    <div>
       <div
         id="indicator"
         style={{
@@ -23,22 +156,8 @@ const MakeSortable = ({
           width: 300,
         }}
       ></div>
-      <div id="box">
-        {items.map((arr, i) => {
-          return (
-            <Box
-              key={arr}
-              id={arr}
-              index={i}
-              array={items}
-              setArray={setItems}
-              indicatorPosition={indicatorPosition}
-              setIndicatorPosition={setIndicatorPosition}
-            />
-          );
-        })}
-      </div>
-    </>
+      <div id="box">{children}</div>
+    </div>
   );
 };
 
